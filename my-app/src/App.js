3d433bea0ebbcbf22a839c3155e0a64f55fef948
alec-deng <alexdeng901@gmail.com>
2021-07-30 20:19:34 -0400

    new

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import grid from './Grid.js';
+import Grid from './Grid.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        grid()
+        <Grid/>
       </div>
     );
   }
--- my-app/src/Grid.js
+++ my-app/src/Grid.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const grid = function() {
-  <div className="grid">
-    <div className="board"></div>
-    <div className="board"></div>
-    <div className="board"></div>
-    <div className="board"></div>
-    <div className="board"></div>
-    <div className="board"></div>
-    <div className="board"></div>
-    <div className="board"></div>
-    <div className="board"></div>
-  </div>
+const Grid = (props) => {
+  const arr = [0,1,2,3,4,5,6,7,8];
+  return (
+    <div className='grid'>
+      {
+      arr.map((e) => {
+        return <div className='board' key={e}></div>
+      })}
+    </div>
+  );
 }
