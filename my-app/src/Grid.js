ad0ddd7f96e23e5455575e649d65611a55eb851f
alec-deng <alexdeng901@gmail.com>
2021-07-30 21:10:07 -0400

    new

--- my-app/src/Grid.js
+++ my-app/src/Grid.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './Grid.css';
 
 const Grid = (props) => {
   const arr = [0,1,2,3,4,5,6,7,8];
+  console.log(arr);
+  const click = () => {
+
+  }
   return (
     <div className='grid'>
-      {
-      arr.map((e) => {
-        return <div className='board' key={e}></div>
+      {arr.map((e) => {
+        return <div className='board' key={e} onClick={click}></div>
       })}
     </div>
   );
